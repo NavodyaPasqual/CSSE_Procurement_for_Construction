cf73c9f0559e08678e510cd0ccd0dfe52fe7eb34
Buddhi Amarasinghe <68139319+GayashaBudddhi@users.noreply.github.com>
2021-10-05 11:27:43 +0530

    Update listOfSites.js
    modified.

--- frontend/src/sites/listOfSites.js
+++ frontend/src/sites/listOfSites.js
@@ -19,11 +19,11 @@ const ListOfSites = () => {
     } = values;
 
     const search = () => {
-        return site.filter((item) => {
-            if (item.region === filterParam) {
+        return site.filter((items) => {
+            if (items.region === filterParam) {
                 return searchParam.some((newItem) => {
                     return (
-                        item[newItem]
+                        items[newItem]
                             .toString()
                             .toLowerCase()
                             .indexOf(q.toLowerCase()) > -1
